9726504393d28522b08f8e389b009dffcc1300db
Chris McLane <72411419+cmclane99@users.noreply.github.com>
2021-10-12 12:30:06 -0400

    Fixed the way app behaves when an existing username is found (for real)
    App no loner redirects user to homepage if same username is found. Displays error message instead

--- help-hero-front-end/app/src/main/java/com/example/helphero/SignUpActivity.java
+++ help-hero-front-end/app/src/main/java/com/example/helphero/SignUpActivity.java
@@ -1,118 +1,81 @@
-package com.example.helphero;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class SignUpActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-
-        // Create EditText objects to grab all the input fields
-        EditText usernameEditText = (EditText)findViewById(R.id.username);
-        EditText passwordEditText = (EditText)findViewById(R.id.password);
-        EditText contactNameEditText = (EditText)findViewById(R.id.emergencyContactName);
-        EditText contactNumberEditText = (EditText)findViewById(R.id.emergencyContactNumber);
-        EditText contactRelationshipEditText = (EditText)findViewById(R.id.emergencyContactRelationship);
-
-        // Create TextView object for errorMessage
-        TextView errorMessageTextView = (TextView)findViewById(R.id.errorMessage);
-
-        // Create button object to handle the onClick
-        Button finishButton = (Button)findViewById(R.id.finishButton);
-
-        finishButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // Store input from EditText boxes into variables
-                String username = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                String contactName = contactNameEditText.getText().toString();
-                String contactNumber = contactNumberEditText.getText().toString();
-                String contactRelationship = contactRelationshipEditText.getText().toString();
-
-                // Check for input errors when button is clicked
-                boolean isValid = true;
-
-                    // If there are errors display error in the errorMessageTextView
-                    if (username == null ||
-                            password == null ||
-                            contactName == null ||
-                            contactNumber == null ||
-                            contactRelationship == null) {
-                        isValid = false;
-                        errorMessageTextView.setText("Please fill out all fields");
+@@ -70,11 +74,12 @@ public void onClick(View v) {
                     }
 
                 // Check to make sure password is within limits
                 if (password.length() <= 8 || password.length() > 15) {
                     isValid = true;
+                if (password.length() < 8 || password.length() >= 15) {
+                    errorMessageTextView.setText("Password must be at least 8 characters and less than 15");
                 }
                 else
                     errorMessageTextView.setText("Password must be at least 8 characters and less than 15");
+                    isValid = true;
+
 
 
                 // Check to make sure password contains a number special character
-                //Pattern pattern = Pattern.compile("[a-zA-Z0-9\\-#\\.\\(\\)\\/%&\\s]");
-                Pattern pattern = Pattern.compile("[\\-#\\.\\*\\&\\%\\#\\$\\(\\)\\/%&\\s]");
-                Matcher matcher = pattern.matcher(password);
-                boolean pwContainsSpecialCharacter = matcher.find();
-                if(!pwContainsSpecialCharacter) {
-                    isValid = false;
+@@ -87,45 +92,44 @@ public void onClick(View v) {
                     errorMessageTextView.setText("Password must contain a number or special character - # . ( ) / % & s]");
                 }
 
                     if(isValid ==true)
                     {
+                    if(isValid == true) {
                         RequestQueue queue = Volley.newRequestQueue(SignUpActivity.this);
                         String url = "http://54.86.66.229:8000/api/create-user/";
+                        String create_userURL = "http://54.86.66.229:8000/api/create-user/";
 
                         JSONObject user = new JSONObject();
 
                         try{
+                        try {
                             user.put("username", username);
                             user.put("password",password);
                             user.put("EmergencyContactNameOne",contactName);
                             user.put("EmergencyContactRelationOne",contactRelationship);
+                            user.put("password", password);
+                            user.put("EmergencyContactNameOne", contactName);
+                            user.put("EmergencyContactRelationOne", contactRelationship);
                             user.put("EmergencyContactPhoneOne", contactNumber);
                         }catch(JSONException e){
                             Toast.makeText(SignUpActivity.this, "ERROR",Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            Toast.makeText(SignUpActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
                         }
 
                         JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, user, new Response.Listener<JSONObject>() {
+                        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, create_userURL, user, new Response.Listener<JSONObject>() {
+
                             @Override
                             public void onResponse(JSONObject response) {
                                 //Toast.makeText(SignUpActivity.this, response.toString(),Toast.LENGTH_LONG).show();
+                                // No errors, proceed to home page
+                                Toast.makeText(SignUpActivity.this, "Success!", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
                             }
                         }, new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
                                 Toast.makeText(SignUpActivity.this, "Something went wrong.",Toast.LENGTH_LONG).show();
+                                // Error, username already exists in database
+                                Toast.makeText(SignUpActivity.this, "Username is already taken!", Toast.LENGTH_LONG).show();
 
                             }
                         });
@@ -120,10 +83,13 @@ public class SignUpActivity extends AppCompatActivity {
 
                         // If no errors redirect to the homepage
                         startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                        queue.add(request);
                     }
+
             }
         });
 
 
     }
+
 }

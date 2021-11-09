d2edce7e1598e8c0a27eb105271bdad5c58a83df
Chris McLane <cmclane1@emich.edu>
2021-11-09 00:46:47 +0000

    impl: Add Function to Edit User Contacts
    - Added updateContacts function in views.py which allows a user to edit their existing contact info
    - Added a new url in urls.py for updating user contact info which will be used by the frontend to send requests to the backend
    - Added a new serializer class in serializer.py for updating contact info
    - Removed TaskList table in models.py

--- backend/HelpHeroServer/api/urls.py
+++ backend/HelpHeroServer/api/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('user-list/', views.listUsers, name="User_list"),
-    path('user-detail/<str:pk>', views.findUser, name="Get_User"),
+    path('user-detail/<str:pk>/', views.findUser, name="Get_User"),
     path('create-user/', views.createUser, name="Create_User"),
     path('login/', views.loginUser, name="User_login"),
+    path('edit-contacts/<str:pk>/', views.updateContacts, name="Edit_contacts")
     
 ]

9fa021f21e6be71c3b3ccb441a446e59b11f0805
root <root@DESKTOP-6FNB2NU.localdomain>
2021-12-04 18:55:13 -0500

    impl: File Read/Write
    -App writes to file when "Save Favorites" button is pressed
    -App able to show stored data after an app restart
    -Able to distinguish file between users
    
    Work for Issue#47: Resource page Favorites

--- help-hero-front-end/app/src/main/java/com/example/helphero/ui/resources/ResourcesFragment.java
+++ help-hero-front-end/app/src/main/java/com/example/helphero/ui/resources/ResourcesFragment.java
@@ -24,6 +24,13 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.helphero.R;
 import com.example.helphero.SignInActivity;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResourcesFragment extends Fragment {
 
     private ResourcesViewModel resourcesViewModel;
@@ -39,6 +46,7 @@ public class ResourcesFragment extends Fragment {
         SharedPreferences.Editor editor = sharedpreferences.edit();
 
         String username = sharedpreferences.getString("username", "user");
+        String filename = username + "HHFavorites.txt";
 
         //find Resource page elements
         CheckBox box1 = (CheckBox)root.findViewById(R.id.check1);
@@ -59,12 +67,41 @@ public class ResourcesFragment extends Fragment {
 
         //collect elements into an array
         CheckBox[] allBoxes = {box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15};
-        int [] allStrings = {R.string.AFSP, R.string.SAMHSA, R.string.NIMH, R.string.Healthline, R.string.SPL,
-                R.string.NAMI, R.string.CDC, R.string.SPTS, R.string.NAASP, R.string.SPRC, R.string.CTL,
-                R.string.BT1T, R.string.SAVE, R.string.TTP, R.string.ZS};
+        ArrayList<Integer> allStrings = new ArrayList<Integer>();
+        allStrings.add(R.string.AFSP);
+        allStrings.add(R.string.SAMHSA);
+        allStrings.add(R.string.NIMH);
+        allStrings.add(R.string.Healthline);
+        allStrings.add(R.string.SPL);
+        allStrings.add(R.string.NAMI);
+        allStrings.add(R.string.CDC);
+        allStrings.add(R.string.SPTS);
+        allStrings.add(R.string.NAASP);
+        allStrings.add(R.string.SPRC);
+        allStrings.add(R.string.CTL);
+        allStrings.add(R.string.BT1T);
+        allStrings.add(R.string.SAVE);
+        allStrings.add(R.string.TTP);
+        allStrings.add(R.string.ZS);
+
+        try{
+            FileInputStream getFile = root.getContext().openFileInput(filename);
+
+            int read = -1;
+            StringBuffer buffer = new StringBuffer();
+            while ((read = getFile.read()) != -1){
+                buffer.append((char)read);
+            }
+
+            String fileText = buffer.toString();
+            Toast.makeText(root.getContext(), fileText, Toast.LENGTH_LONG).show();
+        }
+        catch(IOException e){
+            Toast.makeText(root.getContext(), "Error getting file!", Toast.LENGTH_LONG).show();
+        }
 
         for (int x = 0; x < 15; x++){
-            allBoxes[x].setText(allStrings[x]);
+            allBoxes[x].setText(allStrings.get(x));
         }
 
         Button saveFavorites = (Button) root.findViewById(R.id.saveFav);
@@ -72,18 +109,30 @@ public class ResourcesFragment extends Fragment {
         saveFavorites.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                String filename = username + "HHFavorites.txt";
                 String favNames = "";
 
                 for(CheckBox link: allBoxes){
                     if (link.isChecked()){
                         String linkAddress = link.getText().toString();
 
-                        favNames = favNames + linkAddress + "|";
+                        for(int i = 0; i < 15; i++){
+                            if (linkAddress.equals(getString(allStrings.get(i)))){
+                                favNames = favNames + Integer.toString(i) + "|";
+                            }
+                        }
                     }
                 }
 
-
+                try {
+                    FileOutputStream favFileWrite = root.getContext().openFileOutput(filename, Context.MODE_PRIVATE);
+                    byte[] byteArray = favNames.getBytes();
+                    favFileWrite.write(byteArray);
+                    favFileWrite.close();
+                    Toast.makeText(root.getContext(), "File change success!", Toast.LENGTH_LONG).show();
+                }
+                catch(IOException e){
+                    Toast.makeText(root.getContext(), "File change not possible.", Toast.LENGTH_LONG).show();
+                }
             }
         });
 

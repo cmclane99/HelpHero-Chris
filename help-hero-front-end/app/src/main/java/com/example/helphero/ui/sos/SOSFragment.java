075c4fb7d50401c4f49594aa285158a628425fa6
TLjanelle <44989804+TLjanelle@users.noreply.github.com>
2021-12-05 21:19:48 -0500

    impl: SOS text and Auto Dial 911
    1. Included a SOS text box to display the same PSA message to prevent self hamr
    2. Added a button to Call 911
    3. Upon clicking the button the users phone app will open so the user can click call and immediately call 911 in emergencies

--- help-hero-front-end/app/src/main/java/com/example/helphero/ui/sos/SOSFragment.java
+++ help-hero-front-end/app/src/main/java/com/example/helphero/ui/sos/SOSFragment.java
@@ -1,9 +1,12 @@
 package com.example.helphero.ui.sos;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +26,19 @@ public class SOSFragment extends Fragment {
         SOSViewModel = new ViewModelProvider(this).get(SOSViewModel.class);
         View root = inflater.inflate(R.layout.fragment_sos, container, false);
 
+        // buttonCall = findViewById(R.id.AutoDIal911);
+        Button buttonCall = (Button)root.findViewById(R.id.AutoDial911);
+
+        buttonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:911"));
+                startActivity(intent);
+            }
+        });
+        
         return root;
     }
+
 }
